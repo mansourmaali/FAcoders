c571fcd79af95a971f341e75efa7b451cad100bd
mansour maali <44048554+mansourmaali@users.noreply.github.com>
2018-11-11 09:01:58 +0200

    Update week4-2.py

--- python/week4-2.py
+++ python/week4-2.py
@@ -1,11 +1,8 @@
 print('Numbers from 1 to 10')
-x=6
-input('Guess the number:')
-while int(input('Guess the number:'))!=x:
-    if 1< int(input('Guess the number:'))<10:
-        continue
-    else:
-        print(input('Guess the number:'))
-        continue
+num =6
+num2=int(input('guess the number: '))
+input('Guess the number: '))
+while num !=num2 :
+    num2=int(input('guess the number: '))
 else:
-    print('Great you did it')
+    print("Great you did it")

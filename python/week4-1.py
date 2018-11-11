444a587c634543ead06ac209fa86cb92f33f732d
mansour maali <44048554+mansourmaali@users.noreply.github.com>
2018-11-11 08:54:30 +0200

    Update week4-1.py

--- python/week4-1.py
+++ python/week4-1.py
@@ -1,5 +1,5 @@
-input ('enter a number:')
-if (number%2==0):
-    print('number is even')
+number= int(input ("enter a number:"))
+if number%2==0 :
+    print("number is even")
 else:
-    print('number is odd')
+    print("number is odd")
